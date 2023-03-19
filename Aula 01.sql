ef22300f9ec5d7bc2ffd37806d0c03be9bd36889
Francisco Lucas Sens <franciscosensaulas@gmail.com>
2023-03-19 17:39:02 -0300

    Adicionado exemplos de Operador Lógico E, between e in

--- Aula 01.sql
+++ Aula 01.sql
@@ -178,3 +178,25 @@ SELECT id, nome, data_nascimento
     WHERE 
         YEAR(data_nascimento) >= 1990 AND 
         YEAR(data_nascimento) <= 1999;
+
+-- OPERADOR LÓGICO OR
+-- Consultar os cursos com o nome C# ou Python
+SELECT id, nome FROM cursos WHERE nome = "C#" OR nome = "Python";
+
+-- Consultar os cursos com o nome C# ou Pyhon ou Java
+SELECT id, nome FROM cursos WHERE nome IN ("C#", "Python", "Java");
+
+-- Consultar os cursos com o nome diferente de C# e Python e Java (SEM NOT IN)
+SELECT id, nome FROM cursos WHERE nome != "C#" AND nome != "Python" AND nome != "Java";
+
+-- Consultar os cursos com o nome diferente de C# e Python e Java (COM NOT IN)
+SELECT id, nome FROM cursos WHERE nome NOT IN ("C#", "Python", "Java");
+
+-- Consultar os cursos com carga horária entre 40 e 45 horas
+SELECT id, nome, carga_horaria 
+    FROM cursos 
+    WHERE carga_horaria >= 40 AND carga_horaria <= 45; -- Sem BETWEEN
+
+SELECT id, nome, carga_horaria 
+    FROM cursos
+    WHERE carga_horaria BETWEEN 40 AND 45; -- Com Between
