bb3c1d1d5b22c5c4ad9e8480b91c404aab1e5882
Francisco Lucas Sens <franciscosensaulas@gmail.com>
2023-03-19 18:37:10 -0300

    Enunciado de exercício 01

---
+++ Exercicio01.sql
@@ -0,0 +1,24 @@
+-- Enunciado: Criar tabela de estoques
+--      id: INT
+--      produto: VARCHAR(50)
+--      quantidade: INT
+--      preco_unitario: FLOAT(6, 2)
+--      data_vencimento: DATE
+--      categoria: VARCHAR(30)
+
+-- Inserir 7 registros
+--      2 registros de frutas
+--      2 registros de lanches
+--      2 registros de bebidas
+--      1 registro de produtos de limpeza
+
+-- Categorias [Frutas, Lanches, Bebidas e Produtos de Limpeza]
+
+-- OBS.: PROIBIDO A UTILZIÇÃO DE * EM CONSULTAS SELECT
+-- Consultar todos as colunas registros
+-- Consultar o menor preço
+-- Consultar a maior quantidade
+-- Consultar nome, quantidade, preço unitário e valor dos produtos
+-- Consultar soma total dos valores dos produtos(quanitdade multiplicando preço unitário)
+-- Consultar os produtos da categoria bebidas ordenando pela menor quantidade
+-- Consutlar os produtos de categoria frutas e preço unitário maior que R$ 10,00
